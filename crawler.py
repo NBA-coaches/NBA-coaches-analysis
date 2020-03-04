d9b85657247afb5ebbd8f0e3e27eb7b3e847d1dd
RonenW <44970100+RonenW@users.noreply.github.com>
2020-03-04 22:57:07 +0200

    commit

--- crawler.py
+++ crawler.py
@@ -1,3 +1,10 @@
 from bs4 import BeautifulSoup as bs
+import requests
+import selenium
 
 
+def urlRequest(url):
+    return requests.get(url)
+
+def parseHTML(req):
+    return bs(open(req), "html.parser")
