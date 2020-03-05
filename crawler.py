2f088e2cb61166fb4bbcf11c254f89f1d51a408a
RonenW <44970100+RonenW@users.noreply.github.com>
2020-03-05 08:12:10 +0200

    commit

--- crawler.py
+++ crawler.py
@@ -4,7 +4,23 @@ import selenium
 
 
 def urlRequest(url):
-    return requests.get(url)
+    return requests.get(url).text
 
 def parseHTML(req):
-    return bs(open(req), "html.parser")
+    return bs(req, "html.parser")
+
+def getTeams(soup):
+    teamsList = []
+    for a in soup.findAll('table')[0].find_all('a'):
+        teamsList.append(a['href'][7:10])
+    return teamsList
+
+def getTeamPage(url):
+    soup = parseHTML(urlRequest(url))
+    #TODO
+
+
+
+req = urlRequest("http://www.basketball-reference.com/teams")
+soup = parseHTML(req)
+print(getTeams(soup))
